b562a74430f072effac3912392e81d2a0717cbe6
dyharlan <dyharlan45@gmail.com>
2021-11-29 09:51:00 +0800

    Deprecation of the Swing Front-end

--- src/GachaSim/WishSimMain.java
+++ src/GachaSim/WishSimMain.java
@@ -6,38 +6,31 @@ import static GachaSim.Frontend.WishSimFX.*;
 
 /**
  * The main method calls all the other front-ends that are available to the user.
- * The frontends are Located in GachaSim.Front-end.
+ * The front-ends are Located in GachaSim.Front-end.
  * By default, the simulator uses the JavaFX front-end.
  * @author dyhar
  */
 public class WishSimMain {
 
     /**
-     * @param args This changes what frontend to use for the program.
+     * @param args This changes what front-end to use for the program.
      * Options include:
      * "-c" or "--cli" for the command-line fallback.
-     * "-s" or "--swing" for the Swing front-end.
      * "-f" or "--javafx" for the JavaFX front-end.
      */
     public static void main(String[] args) {
         StringBuilder help = new StringBuilder();
         help.append("\nThese are the valid arguments for GachaSim: \n");
         help.append("--cli or -c: Calls the cli frontend.\n");
-        help.append("--swing or -s: Calls the Swing-based frontend. \n");
         help.append("--javafx or -fx: (Default) Calls the experimental JavaFX-based frontend.\n");
         if (args.length > 0)
             {
                 String one = args[0];
-        
                 switch (one)
                     {
                         case "--cli":
                         case "-c":
                             cli();
-                            break;
-                        case "--swing":
-                        case "-s":
-                            swing();
                             break;
                         case "--javafx":
                         case "-fx":
@@ -49,7 +42,7 @@ public class WishSimMain {
                     }
             }
         else
-            fx();
+            cli();
        
     }
     
