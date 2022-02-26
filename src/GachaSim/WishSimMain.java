404e31c77da5488e8cc07a93240e5ef67e635940
dyharlan <dyharlan45@gmail.com>
2022-02-26 22:10:07 +0800

    Update WishSimMain.java

--- src/GachaSim/WishSimMain.java
+++ src/GachaSim/WishSimMain.java
@@ -1,7 +1,11 @@
 
 package GachaSim;
+import static GachaSim.Backend.GachaPool.*;
 import static GachaSim.Frontend.WishSim.*;
 import static GachaSim.Frontend.WishSimFX.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+//import java.util.Arrays;
 
 /**
  * The main method calls all the other front-ends that are available to the user.
@@ -16,16 +20,26 @@ public class WishSimMain {
      * Options include:
      * "-c" or "--cli" for the command-line fallback.
      * "-f" or "--javafx" for the JavaFX front-end.
+     * 
      */
-    public static void main(String[] args) {
-        StringBuilder help = new StringBuilder();
-        help.append("\nThese are the valid arguments for GachaSim: \n");
-        help.append("--cli or -c: Calls the cli frontend.\n");
-        help.append("--javafx or -fx: (Default) Calls the experimental JavaFX-based frontend.\n");
+    //public static String _5StarCharRateUp = chars[0][chars[0].length-2];
+    //public static String _5StarCharRateUp2 = chars[0][chars[0].length-1];
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        load5StarCharPool("5starchars");
+        load4StarCharPool("4starchars");
+        load5StarWpnPool("5starwpns");
+        load4StarWpnPool("4starwpns");
+        
+        
+        
         if (args.length > 0)
             {
+                StringBuilder help = new StringBuilder();
+                help.append("\nThese are the valid arguments for GachaSim: \n");
+                help.append("--cli or -c: Calls the cli frontend.\n");
+                help.append("--javafx or -fx: (Default) Calls the JavaFX-based frontend.\n");
                 String one = args[0];
-                switch (one)
+                switch (one.toLowerCase())
                     {
                         case "--cli":
                         case "-c":
@@ -44,6 +58,10 @@ public class WishSimMain {
             }
         else
             fx();
+            //cli();
+            //System.out.println(Arrays.deepToString(chars));
+            //System.out.println(Arrays.deepToString(wpns));
+
        
     }
     
