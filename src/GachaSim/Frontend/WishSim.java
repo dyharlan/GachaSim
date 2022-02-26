d7851dcc38a650ee3330d2a42c0fbc45dc2ddc30
dyharlan <dyharlan45@gmail.com>
2022-02-26 22:09:15 +0800

    Update WishSim.java

--- src/GachaSim/Frontend/WishSim.java
+++ src/GachaSim/Frontend/WishSim.java
@@ -1,12 +1,10 @@
 package GachaSim.Frontend;
-
-
-
-
 import GachaSim.Banners.StdBanner;
 import GachaSim.Banners.CharacterBanner;
 import GachaSim.Banners.WeaponBanner;
+import static GachaSim.Backend.GachaPool.*;
 import java.util.Scanner;
+import java.util.Arrays;
 /**
  * TODO: SIMPLIFY/STREAMLINE LOOKUP TABLES FOR THE GACHA POOL [Partially Done, Decoupled Gacha Pool from backend] 
  * TODO: FIND SOMEONE TO VERIFY THE ACCURACY OF THE RATES (From my own testing with n=1000, the accumulated rate (1.6%) matches that of what the game presents.)
@@ -17,68 +15,123 @@ import java.util.Scanner;
 public class WishSim 
 {
     public static void cli() {
-        //Creating an instance of the Character Banner Class
-        //in order to check the counters for the 5 and 4 star pity, and the 5* guarantee.
-        CharacterBanner CbannerStatus = new CharacterBanner(); 
-        //Creating an instance of the Weapon Banner Class
-        //in order to check the counters for the 5 and 4 star pity, and the 5* guarantee.
-        WeaponBanner WbannerStatus = new WeaponBanner();
-        //Creating an instance of the Standard Banner Class
-        //in order to check the counters for the 5 and 4 star pity, and the 5* guarantee.
-        StdBanner SbannerStatus = new StdBanner();
-        //disables the flag to exit the program
+       
         boolean isFinished = false; 
         //declaring a new scanner class.
+        String[] pulls = new String[10];
+        Arrays.fill(pulls, "");
+        int SinglePullCounter=0;
         Scanner input = new Scanner(System.in);
         int choice;
         while(!isFinished) //keeps the program running until the isFinished flag is turned on.
         {
-            System.out.println("\r");
+            //System.out.println("\r");
             //The simulator header. W.I.P.
             System.out.println("Welcome to shit gacha!");
-            System.out.println(" ");
-            System.out.println("Rolls on the Character Banner: " + CbannerStatus.Crolls);
-            System.out.println("Rolls since last 4* on the Character Banner: " + CbannerStatus._4Spity);
-            System.out.println("Guaranteed 5* character? " + CbannerStatus.guaranteed5);
-            System.out.println(" ");
-            System.out.println("Rolls on the Weapon Banner: " + WbannerStatus.Wrolls);
-            System.out.println("Rolls since last 4* on the Character Banner: " + WbannerStatus._4Spity);
             System.out.println(" ");
             //System.out.printf("%.9f\n",Backend.Csoftpity);
             //System.out.printf("%.9f\n",Backend.Wsoftpity);
             //System.out.printf("%.9f\n",Backend.Ssoftpity);
-            System.out.println("Rolls on the Standard Banner: " + SbannerStatus.Srolls);
-            System.out.println("Rolls since last 4* on the Standard Banner: " + SbannerStatus._4Spity);
+            System.out.println("Rolls on the Standard Banner: " + StdBanner.Srolls);
+            System.out.println("Rolls since last 4* on the Standard Banner: " + StdBanner._4Spity);
             System.out.println("What do you want to do?\n1. Roll on the Character Banner Once \n"
                     + "2. Roll on the Weapon Banner Once \n3. Roll on the Standard Banner \n"
                     + "4. Roll on the Character Banner 10x\n5. Roll on the Weapon banner 10x\n"
                     + "6. Roll on the Standard Banner 10x\n7. Exit");
-
+            System.out.println(" ");
+            System.out.println("Rolls on the Character Banner: " + CharacterBanner.Crolls);
+            System.out.println("Rolls since last 4* on the Character Banner: " + CharacterBanner._4Spity);
+            System.out.println("Guaranteed 5* character? " + CharacterBanner.guaranteed5);
+            System.out.println(" ");
+            System.out.println("Rolls on the Weapon Banner: " + WeaponBanner.Wrolls);
+            System.out.println("Rolls since last 4* on the Character Banner: " + WeaponBanner._4Spity);
+            System.out.println(" ");
+            System.out.println(pulls[0]);
+            System.out.println(pulls[1]);
+            System.out.println(pulls[2]);
+            System.out.println(pulls[3]);
+            System.out.println(pulls[4]);
+            System.out.println(pulls[5]);
+            System.out.println(pulls[6]);
+            System.out.println(pulls[7]);
+            System.out.println(pulls[8]);
+            System.out.println(pulls[9]);
+            
             //This switch statement presents all the program functions.
             choice = input.nextInt();
             switch (choice)
             {
                 case 1://Rolls on the character banner "rate up" once
-                    System.out.println(CharacterBanner.CharRoll("Itto", "Xiangling", "Barbara", "Gorou"));
+                    if(SinglePullCounter < pulls.length)
+                    {
+                        pulls[SinglePullCounter] = CharacterBanner.CharRoll(chars[0][chars[0].length-2]);
+                        SinglePullCounter++;
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                    }
+                    else
+                    {
+                        SinglePullCounter = 0;
+                        pulls[SinglePullCounter] = CharacterBanner.CharRoll(chars[0][chars[0].length-2]);
+                        SinglePullCounter++;
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                    }
                     break;
                 case 2:          
-                    System.out.println(WeaponBanner.WpnRoll("Skyward Harp", "RedHorn Stonethresher", "Favonius Lance", "Alley Flash", "Miiternachts Waltz", "The Bell", "Sacrificial Fragments"));
+                    //System.out.println(WeaponBanner.WpnRoll());
+                    if(SinglePullCounter < pulls.length)
+                    {
+                        pulls[SinglePullCounter] = WeaponBanner.WpnRoll();
+                        SinglePullCounter++;
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                    }
+                    else
+                    {
+                        SinglePullCounter = 0;
+                        pulls[SinglePullCounter] = WeaponBanner.WpnRoll();
+                        SinglePullCounter++;
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                    }
                     break;
                 case 3:
-                    System.out.println(StdBanner.StdRoll());
+                    //System.out.println(StdBanner.StdRoll());
+                    if(SinglePullCounter < pulls.length)
+                    {
+                        pulls[SinglePullCounter] = StdBanner.StdRoll();
+                        SinglePullCounter++;
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                    }
+                    else
+                    {
+                        SinglePullCounter = 0;
+                        pulls[SinglePullCounter] = StdBanner.StdRoll();
+                        SinglePullCounter++;
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                    }
                     break;
                 case 4:
-                    for(int ctr = 0; ctr < 10; ctr++)//Rolls on the character banner "rate up" ten times
-                        System.out.println(CharacterBanner.CharRoll("Itto", "Xiangling", "Barbara", "Gorou"));
+                    for(SinglePullCounter = 0; SinglePullCounter < pulls.length; SinglePullCounter++)//Rolls on the character banner "rate up" ten times
+                        pulls[SinglePullCounter] = CharacterBanner.CharRoll(chars[0][chars[0].length-2]);
                     break;
                 case 5:
-                    for(int ctr = 0; ctr < 10; ctr++)//Rolls on the character banner "rate up" ten times
-                        System.out.println(WeaponBanner.WpnRoll("Skyward Harp", "RedHorn Stonethresher", "Favonius Lance", "Alley Flash", "Miiternachts Waltz", "The Bell", "Sacrificial Fragments"));
+                    //for(int ctr = 0; ctr < 10; ctr++)//Rolls on the character banner "rate up" ten times
+                    //    System.out.println(WeaponBanner.WpnRoll());
+                    //break;
+                    for(SinglePullCounter = 0; SinglePullCounter < pulls.length; SinglePullCounter++)//Rolls on the character banner "rate up" ten times
+                        pulls[SinglePullCounter] = WeaponBanner.WpnRoll();
                     break;
                 case 6:
-                    for(int ctr = 0; ctr < 10; ctr++)//Rolls on the character banner "rate up" ten times
-                        System.out.println(StdBanner.StdRoll());
-                     break;
+                    //for(int ctr = 0; ctr < 10; ctr++)//Rolls on the character banner "rate up" ten times
+                    //    System.out.println(StdBanner.StdRoll());
+                    // break;
+                    for(SinglePullCounter = 0; SinglePullCounter < pulls.length; SinglePullCounter++)//Rolls on the character banner "rate up" ten times
+                        pulls[SinglePullCounter] = StdBanner.StdRoll();
+                    break;
                 case 7:
                    isFinished = true;
                    break;
